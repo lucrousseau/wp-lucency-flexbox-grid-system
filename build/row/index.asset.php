767ce0334f094f5120b51fa6d727be851f7d1628
Luc Rousseau <luc.rousseau@strongdeveloppement.ca>
2024-04-30 09:20:46 -0400

    various

--- build/row/index.asset.php
+++ build/row/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '817fcc8ec4047396b90f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'eefc2ea53382bc5408d2');
