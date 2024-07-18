c1c6de961b15613ffb2b1a21a45b1d256b7bf011
Luc Rousseau <luc.rousseau@strongdeveloppement.ca>
2024-07-18 09:19:33 -0400

    Update renderGridFlexSelector.js

--- src/commons/renderGridFlexSelector.js
+++ src/commons/renderGridFlexSelector.js
@@ -20,20 +20,3 @@ export default function renderGridFlexSelector({
 		/>
 	);
 }
-
-export function getDisplayTypeFlags({ display }) {
-	const isGrid = display === "grid";
-	const isFlex = display === "flex";
-	const isCell = display === "cell";
-	const isColumn = display === "column";
-	const isContainer = display === "container";
-
-	return {
-		isGrid,
-		isFlex,
-		isCell,
-		isColumn,
-		isContainer,
-		display,
-	};
-}
