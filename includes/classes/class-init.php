a06dd07225b072065617dedb4479ce46517cf89b
Luc Rousseau <luc.rousseau@strongdeveloppement.ca>
2024-08-06 15:34:54 -0400

    Update class-init.php

--- includes/classes/class-init.php
+++ includes/classes/class-init.php
@@ -3,7 +3,7 @@
  * Initialization file for the Lucency plugin.
  *
  * This file is responsible for initializing core components of the Lucency plugin,
- * including blocks management, block categories, and asset loading and more.
+ * including block management, block categories, and asset loading, among other features.
  *
  * @package Lucency
  */
@@ -23,17 +23,28 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * Handles the initialization of essential components for the Lucency plugin.
  * This includes setting up the Blocks Manager, Blocks Category, and Assets Loader.
+ *
+ * @package Lucency
  */
 class Init {
 	/**
 	 * Init constructor.
 	 *
-	 * Constructs the Init class and initializes the core components:
-	 * - Blocks Manager: Manages the custom Gutenberg blocks.
-	 * - Blocks Category: Defines and registers custom block categories.
-	 * - Assets Loader: Handles the loading of plugin assets such as scripts and styles.
+	 * Constructs the Init class and hooks the initialization method to the 'plugins_loaded' action.
+	 * This ensures that core components of the plugin are properly initialized after plugins are loaded.
 	 */
 	public function __construct() {
+		add_action( 'plugins_loaded', array( $this, 'initialize_plugin' ) );
+	}
+
+	/**
+	 * Initializes core components of the Lucency plugin.
+	 *
+	 * This method is hooked to the 'plugins_loaded' action and is responsible for creating instances
+	 * of the Blocks Manager, Blocks Category, and Assets Loader classes. These components manage
+	 * custom Gutenberg blocks, register custom block categories, and handle the loading of plugin assets.
+	 */
+	public function initialize_plugin() {
 		new Manager();
 		new Category();
 		new Loader();
